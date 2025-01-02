a2edeb5f2243c65897005f7d32e16064ca896cfe
rahulidea <rahulideasetters@gmail.com>
2025-01-03 00:03:48 +0530

    Update CheckSchoolId.php

--- app/Http/Middleware/CheckSchoolId.php
+++ app/Http/Middleware/CheckSchoolId.php
@@ -22,7 +22,7 @@ class CheckSchoolId
     header('Access-Control-Allow-Methods: GET, POST, PUT');  // Allow certain HTTP methods
 
     // Check for 'school_id' header
-    dd($request->header('school_id'));  // For debugging purposes
+    dd(dd($request->headers->all()));  // For debugging purposes
         if (!$request->hasHeader('school_id') || !$request->header('school_id')) {
             return response()->json([
                 'status' => false,
