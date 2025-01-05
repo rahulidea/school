e7ed20f9e2c26302cdb3ed51f61b9e1b841dc3c1
rahulidea <rahulideasetters@gmail.com>
2025-01-06 00:51:06 +0530

    Test commit

--- app/Http/Middleware/CheckSchoolId.php
+++ app/Http/Middleware/CheckSchoolId.php
@@ -17,21 +17,19 @@ class CheckSchoolId
     public function handle(Request $request, Closure $next)
     {
 
-        // Set CORS headers to allow all origins, methods, and headers
-    $response = $next($request);
-    $response->headers->set('Access-Control-Allow-Origin', '*');
-    $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization, Origin');
-
-    // Handle OPTIONS request for pre-flight requests
-    if ($request->getMethod() == 'OPTIONS') {
-        return response('', 200)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization, Origin');
-    }
-
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization, Origin, school_id');
     
+        // Handle OPTIONS requests for pre-flight checks
+        if ($request->getMethod() == 'OPTIONS') {
+            return response('', 200)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization, Origin, school_id');
+        }
+
+
         if (!$request->hasHeader('school_id') || !$request->header('school_id')) {
             return response()->json([
                 'status' => false,
